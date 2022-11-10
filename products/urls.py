6e66866509f3dace8f7b1e972f49ab207361468b
RoHiTMeKhE <rohitmekhe4438@gmail.com>
2022-11-10 22:57:32 +0530

    first

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path
+from . import views
+urlpatterns = [
+ path('', views.index, name="index"),
+ path('about', views.about, name="about"),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
